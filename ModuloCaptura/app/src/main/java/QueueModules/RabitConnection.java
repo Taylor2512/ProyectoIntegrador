d14262b73f1076c96375b25fdaf48cd867ba5bba
Taylor2512 <83801946+Taylor2512@users.noreply.github.com>
2023-12-05 08:47:12 -0500

    agregar modulos

---
+++ ModuloCaptura/app/src/main/java/QueueModules/RabitConnection.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package QueueModules;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import java.io.IOException;
+import java.io.IOException;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeoutException;
+
+/**
+ *
+ * @author jt251
+ */
+public class RabitConnection {
+
+     private Connection connection;
+
+    private void conectar() {
+        if (connection == null || !connection.isOpen()) {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("localhost"); // Reemplaza esto con la dirección de tu servidor RabbitMQ
+
+            try {
+                connection = factory.newConnection();
+            } catch (IOException | TimeoutException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void enviarMensaje(String mensaje, String NOMBRE_COLA) {
+        conectar();
+        try (Channel channel = connection.createChannel()) {
+            // Declara la cola a la que enviarás los mensajes
+            channel.queueDeclare(NOMBRE_COLA, false, false, false, null);
+
+            // Publica el mensaje en la cola
+            channel.basicPublish("", NOMBRE_COLA, null, mensaje.getBytes());
+            System.out.println(" [x] Mensaje enviado: '" + mensaje + "'");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        cerrarConexion();
+    }
+
+   public String recibirMensaje(String NOMBRE_COLA)  {
+               conectar();
+
+      try (Channel channel = connection.createChannel()) {
+          
+        final BlockingQueue<String> queue = new ArrayBlockingQueue<>(1);
+
+        // Se configura el consumidor para recibir el mensaje
+        channel.basicConsume(NOMBRE_COLA, true, (consumerTag, delivery) -> {
+            String mensajeRecibido = new String(delivery.getBody(), "UTF-8");
+            queue.offer(mensajeRecibido);
+        }, consumerTag -> {});
+
+        // Se espera a recibir un mensaje
+        return queue.take();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+      cerrarConexion();
+        return "";
+    }
+
+    public void cerrarConexion() {
+        try {
+            if (connection != null && connection.isOpen()) {
+                connection.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
